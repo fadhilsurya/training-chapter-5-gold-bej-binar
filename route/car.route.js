2e937a53fc1a8b5861fbf6c81c8b952baec45717
Muhamad Fadhil Surya Putra <fadhilsurya1@gmail.com>
2023-02-03 01:57:02 +0700

    merapihkan code

--- route/car.route.js
+++ route/car.route.js
@@ -3,10 +3,7 @@ const car = require('../controller/car.controller')
 const { body, validationResult } = require('express-validator')
 
 // in this file we separate router to from the main /user address to smaller one such as
-// /dashboard /register /signin /signup /etc
 const carClass = new car()
-
-// bagian ini me-render bagian page menggunakan EJS 
 router.get('/', (req, res, next) => {
 
     carClass.getAllCar()
@@ -98,33 +95,5 @@ router.delete('/:id', (req, res, next) => {
 
         })
 })
-
-
-
-// router.get('/signin', (req, res, next) => {
-//     res.render('signin')
-// })
-
-// router.get('/signup', (req, res, next) => {
-//     res.render('signup')
-// })
-
-// menghubungkan ke controller
-// router.post('/signup',
-//     body('password').isLength({ min: 5 }),
-//     body('password').isString(),
-//     body('email').isEmail(),
-//     (req, res, next) => {
-//         const errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//             return res.status(400).json({ errors: errors.array() });
-//         }
-
-//         userClass.signup(req.body.email, req.body.password)
-
-//         return res.status(200).json({
-//             data: 'successfully inserted'
-//         })
-//     })
 
 module.exports = router
